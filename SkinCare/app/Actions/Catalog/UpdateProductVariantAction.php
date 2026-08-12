8d31cf74f350049cb43bdfd790deaed1a5641c7c
generalali110-akd <generalali110@gmail.com>
2026-08-13 00:13:59 +0330

    style(backend): format variant update action

--- SkinCare/app/Actions/Catalog/UpdateProductVariantAction.php
+++ SkinCare/app/Actions/Catalog/UpdateProductVariantAction.php
@@ -11,9 +11,7 @@ use Illuminate\Validation\ValidationException;
 
 final class UpdateProductVariantAction
 {
-    public function __construct(private readonly AuditLogger $auditLogger)
-    {
-    }
+    public function __construct(private readonly AuditLogger $auditLogger) {}
 
     public function execute(
         ProductVariant $variant,
