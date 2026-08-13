23786c3d6473ad93758949880aa88d5c0a8df7e9
generalali110-akd <generalali110@gmail.com>
2026-08-13 12:51:52 +0330

    test(e2e): bind test gateways only in testing environment

--- SkinCare/app/Providers/AppServiceProvider.php
+++ SkinCare/app/Providers/AppServiceProvider.php
@@ -8,6 +8,8 @@ use App\Infrastructure\Payments\NullPaymentGateway;
 use App\Infrastructure\Payments\ZarinpalPaymentGateway;
 use App\Infrastructure\Sms\NullSmsGateway;
 use App\Infrastructure\Sms\SmsIrGateway;
+use App\Infrastructure\Testing\E2ePaymentGateway;
+use App\Infrastructure\Testing\E2eSmsGateway;
 use Illuminate\Support\ServiceProvider;
 use LogicException;
 
@@ -19,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
             return match (config('sms.driver')) {
                 'null' => new NullSmsGateway,
                 'smsir' => new SmsIrGateway((array) config('sms.smsir', [])),
+                'e2e' => $this->app->environment('testing')
+                    ? new E2eSmsGateway
+                    : throw new LogicException('The E2E SMS driver is restricted to the testing environment.'),
                 default => throw new LogicException('Unsupported SMS driver configured.'),
             };
         });
@@ -27,6 +32,9 @@ class AppServiceProvider extends ServiceProvider
             return match (config('payment.driver')) {
                 'null' => new NullPaymentGateway,
                 'zarinpal' => new ZarinpalPaymentGateway((array) config('payment.zarinpal', [])),
+                'e2e' => $this->app->environment('testing')
+                    ? new E2ePaymentGateway
+                    : throw new LogicException('The E2E payment driver is restricted to the testing environment.'),
                 default => throw new LogicException('Unsupported payment driver configured.'),
             };
         });
