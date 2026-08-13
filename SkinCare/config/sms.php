992eccbdfd6f370bec4559aced6e88331b25d553
generalali110-akd <generalali110@gmail.com>
2026-08-13 05:29:44 +0330

    config(backend): add SMS outbox retry policy

--- SkinCare/config/sms.php
+++ SkinCare/config/sms.php
@@ -10,4 +10,9 @@ return [
         'requests_per_mobile_10_minutes' => (int) env('OTP_REQUESTS_PER_MOBILE_10_MINUTES', 5),
         'requests_per_ip_10_minutes' => (int) env('OTP_REQUESTS_PER_IP_10_MINUTES', 20),
     ],
+    'outbox' => [
+        'max_attempts' => (int) env('SMS_OUTBOX_MAX_ATTEMPTS', 8),
+        'lock_ttl_seconds' => (int) env('SMS_OUTBOX_LOCK_TTL_SECONDS', 300),
+        'notification_expire_hours' => (int) env('SMS_NOTIFICATION_EXPIRE_HOURS', 24),
+    ],
 ];
