0b0432c02539eaaae924f1a91eeafdb94d0765e8
generalali110-akd <generalali110@gmail.com>
2026-08-14 11:50:58 +0330

    config(media): define explicit local and public storage

---
+++ SkinCare/config/filesystems.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'default' => env('FILESYSTEM_DISK', 'local'),
+
+    'disks' => [
+        'local' => [
+            'driver' => 'local',
+            'root' => storage_path('app/private'),
+            'serve' => false,
+            'throw' => true,
+        ],
+        'public' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public'),
+            'url' => rtrim((string) env('APP_URL', 'http://localhost'), '/').'/storage',
+            'visibility' => 'public',
+            'throw' => true,
+        ],
+    ],
+];
